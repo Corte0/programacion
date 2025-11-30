a447b71a174c836207aebd7ac3117d67a05df9d8
Luciano Cortesini <cortesiniluciano@gmail.com>
2025-11-29 22:35:43 -0300

    actualiza 4.b

--- TP2/funciones_problemas_tecnicos/b.py
+++ TP2/funciones_problemas_tecnicos/b.py
@@ -1,16 +1,19 @@
 import math
-from astropy.constants import GM_earth
-from astropy.constants import R_earth
+from astropy import constants as c
 from astropy import units as u
 import numpy as np
 
 def calcular_velocidades(ha_km: float, phi_deg:float, hb_km:float):
     '''
+    args:
+    ha_km: altura inicial en km
+    phi_deg: ángulo de ingreso con respecto a la dirección radial terrestre
+    hb_km: altura de órbita de aterrizaje en km
     '''
-    ha = (ha_km * u.km).to(u.m) + R_earth
-    hb = (hb_km * u.km).to(u.m) + R_earth
+    ha = (ha_km * u.km).to(u.m) + c.R_earth
+    hb = (hb_km * u.km).to(u.m) + c.R_earth
     phi = phi_deg * u.deg
-    vaf = (2 * GM_earth * (ha**(-1) - hb**(-1)) * (1 - (ha / (hb * np.sin(phi)))**2)**(-1))**(1/2)
+    vaf = (2 * c.GM_earth * (ha**(-1) - hb**(-1)) * (1 - (ha / (hb * np.sin(phi)))**2)**(-1))**(1/2)
     vbf = ha / (hb * np.sin(phi)) * vaf
     return vaf.value, vbf.value
 
